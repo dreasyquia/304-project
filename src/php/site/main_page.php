5678d7cb29a0095a8fcaf76b6cf5acfd102859a7
dreasyquia <101891554+dreasyquia@users.noreply.github.com>
2023-06-16 13:18:21 -0600

    Add files via upload

---
+++ src/php/site/main_page.php
@@ -0,0 +1,27 @@
+<html>
+    <head>
+        <title>WedUP</title>
+        <link rel="stylesheet" href="style.php">
+    </head>
+    <body>
+        <h1 class="main-page-header">MAIN PAGE</h1>
+        <div>
+            <a href="add_wedding.php" class="main-button">Add new wedding</a>
+            <br/>
+            <a href="guests.php" class="main-button">Add guests to a wedding</a>
+            <br/>
+            <a href="staff.php" class="main-button">Add staff to a wedding</a>    
+            <br/>
+            <a href="view_weddings.php" class="main-button">View wedding info</a>
+            <br/>
+            <a href="update_delete.php" class="main-button">Update or delete wedding info</a>
+            <br/>
+            <a href="search.php" class="main-button">Search and filter through weddings</a>
+            <br/>
+            <a href="venues.php" class="main-button">View available venues</a>
+            <br/>
+            <a href="companies.php"class="main-button">View companies and their hourly rates</a>
+            <br/>
+        </div>
+    </body>
+</html>
