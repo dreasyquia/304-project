d22e5a075a6cfee8c82e56fe8b699c0f009f1258
dreasyquia <101891554+dreasyquia@users.noreply.github.com>
2023-06-16 13:19:13 -0600

    Add files via upload

---
+++ src/php/site/css/style.php
@@ -0,0 +1,74 @@
+<?php
+header("Content-type: text/css");
+?>
+
+body {
+    font-family: Georgia, serif;
+    background-color: ivory;
+}
+
+table {
+  width: 75%;
+  border-collapse: collapse;
+  border: 1px solid;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+th {
+  background-color: #ECDFEC;
+  border: 1px solid;
+}
+
+td {
+  text-align: center;
+  border: 1px solid;
+}
+
+input[type=submit] {
+    background-color: #ECDFEC;
+    border: none;
+    color: black;
+    padding: 10px;
+    text-decoration: none;
+    border-radius: 5px;
+    font-family: Georgia, serif;
+    font-weight: bold;
+    font-size: 1em;
+    cursor: pointer;
+}
+
+.main-button {
+    display: block;
+    margin: auto;
+    width: 450px;
+    background: #ECDFEC;
+    padding: 10px;
+    justify-content: center;
+    text-align: center;
+    border-radius: 5px;
+    color: black;
+    font-weight: bold;
+    line-height: 30px;
+}
+
+.side-button {
+    display: block;
+    width: 100px;
+    background: #ECDFEC;
+    padding: 10px;
+    text-align: center;
+    border-radius: 5px;
+    color: black;
+    font-weight: bold;
+    font-size: 1em;
+}
+
+.main-page-header {
+    text-align: center;
+}
+
+a:link { text-decoration: none; }
+a:visited { text-decoration: none; }
+a:hover { text-decoration: none; }
+a:active { text-decoration: none; }
